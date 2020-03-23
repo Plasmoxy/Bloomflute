907da3308fedf3bc0b2ff1dc57fcfa8990257764
Plasmoxy <sebohacker@gmail.com>
2020-03-23 11:03:36 +0100

    hmmm

--- FlutterUdemy/expenses_app/lib/transaction_item.dart
+++ FlutterUdemy/expenses_app/lib/transaction_item.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'model/transaction.dart';
 import 'package:intl/intl.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key key,
     @required this.tx,
@@ -11,6 +13,21 @@ class TransactionItem extends StatelessWidget {
 
   final Transaction tx;
   final Function deleteTransaction;
+
+  @override
+  _TransactionItemState createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  Color avatarColor;
+
+  // note: NO SETSTATE INSIDE INITSTATE !
+  @override
+  void initState() {
+    super.initState();
+    const kolores = [Colors.red, Colors.black, Colors.blue, Colors.purple];
+    avatarColor = kolores[Random().nextInt(kolores.length)];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,25 +39,26 @@ class TransactionItem extends StatelessWidget {
       ),
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: avatarColor,
           radius: 30,
           child: Padding(
             padding: EdgeInsets.all(6),
             child: FittedBox(
-              child: Text('${tx.amount} €'),
+              child: Text('${widget.tx.amount} €'),
             ),
           ),
         ),
         title: Text(
-          tx.title,
+          widget.tx.title,
           style: Theme.of(context).textTheme.title,
         ),
         subtitle: Text(
-          DateFormat("dd. MM. yyyy").format(tx.date),
+          DateFormat("dd. MM. yyyy").format(widget.tx.date),
         ),
         trailing: IconButton(
           icon: Icon(Icons.delete),
           color: Theme.of(context).errorColor,
-          onPressed: () => deleteTransaction(tx.id),
+          onPressed: () => widget.deleteTransaction(widget.tx.id),
         ),
       ),
     );
