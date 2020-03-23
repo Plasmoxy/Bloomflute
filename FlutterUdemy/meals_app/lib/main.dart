46b5aff96c07eb1525528f1e95870efac1954c6f
Plasmoxy <sebohacker@gmail.com>
2020-03-23 12:39:17 +0100

    xd

--- FlutterUdemy/meals_app/lib/main.dart
+++ FlutterUdemy/meals_app/lib/main.dart
@@ -6,26 +6,29 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final theme = ThemeData(
+      primarySwatch: Colors.pink,
+      accentColor: Colors.amber,
+      canvasColor: Color.fromRGBO(255, 254, 229, 1),
+      fontFamily: 'Raleway',
+      textTheme: ThemeData.light().textTheme.copyWith(
+            body1: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            body2: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            title: TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+    );
+
     return MaterialApp(
       title: 'Meals App',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        accentColor: Colors.amber,
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        fontFamily: 'Raleway',
-        textTheme: ThemeData.light().textTheme.copyWith(
-              body1: TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
-              ),
-              body2: TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
-              ),
-              title: TextStyle(
-                fontSize: 24,
-                fontFamily: 'RobotoCondensed',
-              ),
-            ),
-      ),
+      theme: theme,
       home: CategoriesScreen(),
     );
   }
