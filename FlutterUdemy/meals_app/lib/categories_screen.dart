dcab5e6a1e947afbb8711fd41c4eaf0024c1ce4d
Plasmoxy <sebohacker@gmail.com>
2020-03-23 12:06:30 +0100

    lovely

--- FlutterUdemy/meals_app/lib/categories_screen.dart
+++ FlutterUdemy/meals_app/lib/categories_screen.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'model/mockdata.dart';
+import 'components/category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(title: Text('Meal App')),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((cdata) => CategoryItem(cdata.title, cdata.color))
+            .toList(),
       ),
-      children: <Widget>[],
     );
   }
 }
--- FlutterUdemy/meals_app/lib/main.dart
+++ FlutterUdemy/meals_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,28 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Meals App"),
-      ),
-      body: Center(
-        child: Text('yosh'),
-      ),
+      home: CategoriesScreen(),
     );
   }
 }
