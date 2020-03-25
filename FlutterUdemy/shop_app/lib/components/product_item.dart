da33268f37ce177a7d2124d884bc385533855a5c
Plasmoxy <sebohacker@gmail.com>
2020-03-25 16:07:33 +0100

    xd

--- FlutterUdemy/shop_app/lib/components/product_item.dart
+++ FlutterUdemy/shop_app/lib/components/product_item.dart
@@ -8,6 +8,28 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(child: Image.network(product.imageUrl));
+    return GridTile(
+      child: Image.network(
+        product.imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          product.title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: Icon(
+            Icons.shopping_cart,
+          ),
+          onPressed: () {},
+        ),
+        backgroundColor: Colors.black54,
+      ),
+    );
   }
 }
