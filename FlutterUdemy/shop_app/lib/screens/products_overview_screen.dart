bfbb36c327929c605c68f7fcb57cbd52bee5eb92
Plasmoxy <sebohacker@gmail.com>
2020-03-25 20:38:50 +0100

    rf

--- FlutterUdemy/shop_app/lib/screens/products_overview_screen.dart
+++ FlutterUdemy/shop_app/lib/screens/products_overview_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/components/product_item.dart';
-import 'package:shop_app/providers/products.dart';
+import 'package:shop_app/model/products.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
