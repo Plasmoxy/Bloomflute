acad40f0aed1afa40564199c2c24d44d53dcf90c
Plasmoxy <sebohacker@gmail.com>
2020-03-25 20:23:50 +0100

    lmao legit react context and hooks

--- FlutterUdemy/shop_app/lib/screens/products_overview_screen.dart
+++ FlutterUdemy/shop_app/lib/screens/products_overview_screen.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop_app/components/product_item.dart';
-import 'package:shop_app/model/mock.dart';
+import 'package:shop_app/providers/products.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final products = Provider.of<Products>(context).items;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Shop XD'),
       ),
       body: GridView.builder(
         padding: EdgeInsets.all(10),
-        itemCount: MOCK_PRODUCTS.length,
+        itemCount: products.length,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
         ),
-        itemBuilder: (ctx, i) => ProductItem(MOCK_PRODUCTS[i]),
+        itemBuilder: (ctx, i) => ProductItem(products[i]),
       ),
     );
   }
