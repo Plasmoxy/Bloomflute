95d8e22bd1df26bc672be9cfa6cd20fa1c66d5c6
Plasmoxy <sebohacker@gmail.com>
2020-04-03 12:39:26 +0200

    meh

--- FlutterUdemy/shop_app/lib/screens/user_products_screen.dart
+++ FlutterUdemy/shop_app/lib/screens/user_products_screen.dart
@@ -8,6 +8,10 @@ import 'package:shop_app/screens/edit_product_screen.dart';
 class UserProductsScreen extends StatelessWidget {
   static const route = '/user-products';
 
+  Future<void> _refresh(BuildContext ctx) async {
+    await Provider.of<Products>(ctx, listen: false).fetchAndSetProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final products = Provider.of<Products>(context);
@@ -24,17 +28,22 @@ class UserProductsScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView.builder(
-        padding: EdgeInsets.all(10),
-        itemCount: products.items.length,
-        itemBuilder: (bctx, i) => Column(children: <Widget>[
-          UserProductItem(
-            products.items[i].id,
-            products.items[i].title,
-            products.items[i].imageUrl,
-          ),
-          Divider(),
-        ]),
+      body: RefreshIndicator(
+        color: Theme.of(context).canvasColor,
+        backgroundColor: Theme.of(context).accentColor,
+        onRefresh: () => _refresh(context),
+        child: ListView.builder(
+          padding: EdgeInsets.all(10),
+          itemCount: products.items.length,
+          itemBuilder: (bctx, i) => Column(children: <Widget>[
+            UserProductItem(
+              products.items[i].id,
+              products.items[i].title,
+              products.items[i].imageUrl,
+            ),
+            Divider(),
+          ]),
+        ),
       ),
       drawer: AppDrawer(),
     );
