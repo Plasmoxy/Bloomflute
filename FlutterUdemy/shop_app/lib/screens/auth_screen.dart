c5c8e6e84a3f315338edcc621359065a65a0b4a8
Plasmoxy <sebohacker@gmail.com>
2020-04-09 11:15:38 +0200

    Update auth_screen.dart

--- FlutterUdemy/shop_app/lib/screens/auth_screen.dart
+++ FlutterUdemy/shop_app/lib/screens/auth_screen.dart
@@ -91,7 +91,7 @@ class AuthCard extends StatefulWidget {
   _AuthCardState createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.Login;
   Map<String, String> _authData = {
@@ -100,10 +100,25 @@ class _AuthCardState extends State<AuthCard> {
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
+  AnimationController _animCtrl;
+  Animation<double> _heightAnimation;
+
+  @override
+  void initState() {
+    _animCtrl = AnimationController(duration: Duration(milliseconds: 300), vsync: this);
+    super.initState();
+    _heightAnimation = Tween<double>(begin: 260, end: 320).animate(
+      CurvedAnimation(
+        parent: _animCtrl,
+        curve: Curves.fastOutSlowIn,
+      ),
+    );
+  }
 
   @override
   void dispose() {
     _passwordController.dispose();
+    _animCtrl.dispose();
     super.dispose();
   }
 
@@ -174,10 +189,12 @@ class _AuthCardState extends State<AuthCard> {
       setState(() {
         _authMode = AuthMode.Signup;
       });
+      _animCtrl.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
+      _animCtrl.reverse();
     }
   }
 
@@ -189,11 +206,15 @@ class _AuthCardState extends State<AuthCard> {
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        height: _authMode == AuthMode.Signup ? 320 : 260,
-        constraints: BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, ch) => Container(
+          height: _heightAnimation.value,
+          constraints: BoxConstraints(minHeight: _heightAnimation.value),
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: ch,
+        ),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
