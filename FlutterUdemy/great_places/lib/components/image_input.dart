fb34cf60ba4a88dae4835d5a6c2152f1f6f4d20d
Plasmoxy <sebohacker@gmail.com>
2020-04-09 19:48:26 +0200

    Update image_input.dart

--- FlutterUdemy/great_places/lib/components/image_input.dart
+++ FlutterUdemy/great_places/lib/components/image_input.dart
@@ -24,6 +24,8 @@ class _ImageInputState extends State<ImageInput> {
       source: ImageSource.camera,
       maxWidth: 600,
     );
+    if (imageFile == null) return;
+
     setState(() => _storedImage = imageFile);
 
     final appDir = await getApplicationDocumentsDirectory();
