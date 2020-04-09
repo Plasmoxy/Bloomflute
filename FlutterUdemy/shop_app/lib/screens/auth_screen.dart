34d41211b9d4bf2516aa678b0e2a3bf29cfe79e5
Plasmoxy <sebohacker@gmail.com>
2020-04-09 11:26:01 +0200

    hmhm

--- FlutterUdemy/shop_app/lib/screens/auth_screen.dart
+++ FlutterUdemy/shop_app/lib/screens/auth_screen.dart
@@ -101,18 +101,23 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
   var _isLoading = false;
   final _passwordController = TextEditingController();
   AnimationController _animCtrl;
-  Animation<double> _heightAnimation;
+  Animation<Offset> _slideAnimation;
+  Animation<double> _opacityAnimation;
 
   @override
   void initState() {
     _animCtrl = AnimationController(duration: Duration(milliseconds: 300), vsync: this);
     super.initState();
-    _heightAnimation = Tween<double>(begin: 260, end: 320).animate(
+    _slideAnimation = Tween<Offset>(begin: Offset(0, -1.5), end: Offset(0, 0)).animate(
       CurvedAnimation(
         parent: _animCtrl,
         curve: Curves.fastOutSlowIn,
       ),
     );
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+      parent: _animCtrl,
+      curve: Curves.easeIn,
+    ));
   }
 
   @override
@@ -245,20 +250,32 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
                     _authData['password'] = value;
                   },
                 ),
-                if (_authMode == AuthMode.Signup)
-                  TextFormField(
-                    enabled: _authMode == AuthMode.Signup,
-                    decoration: InputDecoration(labelText: 'Confirm Password'),
-                    obscureText: true,
-                    validator: _authMode == AuthMode.Signup
-                        ? (value) {
-                            if (value != _passwordController.text) {
-                              return 'Passwords do not match!';
-                            }
-                            return null;
-                          }
-                        : null,
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 300),
+                  constraints: BoxConstraints(
+                    minHeight: _authMode == AuthMode.Signup ? 60 : 0,
+                    maxHeight: _authMode == AuthMode.Signup ? 120 : 0,
                   ),
+                  child: FadeTransition(
+                    opacity: _opacityAnimation,
+                    child: SlideTransition(
+                      position: _slideAnimation,
+                      child: TextFormField(
+                        enabled: _authMode == AuthMode.Signup,
+                        decoration: InputDecoration(labelText: 'Confirm Password'),
+                        obscureText: true,
+                        validator: _authMode == AuthMode.Signup
+                            ? (value) {
+                                if (value != _passwordController.text) {
+                                  return 'Passwords do not match!';
+                                }
+                                return null;
+                              }
+                            : null,
+                      ),
+                    ),
+                  ),
+                ),
                 SizedBox(
                   height: 20,
                 ),
