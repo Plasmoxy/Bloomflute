d8807debdee5ff8f59502c8af476d39de1ef1e68
Plasmoxy <plasmoxy@gmail.com>
2021-07-23 16:05:46 +0200

    modcubit

--- JobLearn/bloc_00/lib/cubit.dart
+++ JobLearn/bloc_00/lib/cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 
+// cubitttt
 class CounterCubit extends Cubit<int> {
   CounterCubit(int initialState) : super(initialState);
   void increment() => emit(state + 1);
